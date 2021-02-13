f0a12f1b46cc12696c68bed64325afc3a919b0f9
Hiraryo <nintendo640804@gmail.com>
2021-02-14 00:25:54 +0900

    ポーズ処理のプログラムを追加

---
+++ Assets/Script/ShootingScript/PauseButton.cs
@@ -0,0 +1,11 @@
+﻿// It's only manners to tackle all out a game that they have gone all out in creating. Pressing the pause button is just rude!
+using UnityEngine;
+
+public class PauseButton : MonoBehaviour
+{
+    public void TheGameWorld()
+    {
+        //ゲームが進行中であれば一時停止、ゲームが止まっていれば再生
+        Time.timeScale = (Time.timeScale != 0.0f) ? 0.0f : 1.0f;
+    }
+}
