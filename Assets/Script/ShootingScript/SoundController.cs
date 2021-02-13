6cbd0cdd1b22c2048e5401b831cb037f3866e8af
Hiraryo <nintendo640804@gmail.com>
2021-02-13 12:22:37 +0900

    音量調節プログラムを追加

---
+++ Assets/Script/ShootingScript/SoundController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+//AudioSourceコンポーネントを自動的に追加
+[RequireComponent(typeof(AudioSource))]
+public class SoundController : MonoBehaviour
+{
+    //音量
+    private float _volume;
+    private AudioSource _audioSource;
+    void Start()
+    {
+        //スライダーの値を音量に設定
+        _audioSource = this.GetComponent<AudioSource>();
+        _volume = this.GetComponent<Slider>().normalizedValue;
+    }
+
+    void Update()
+    {
+        //スライダーで音量を変更
+        _audioSource.volume = this.GetComponent<Slider>().normalizedValue;
+    }
+}
