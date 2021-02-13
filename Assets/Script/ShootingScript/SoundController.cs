857ebadf3cacd1ed72d089c3c864089c6235d907
Hiraryo <nintendo640804@gmail.com>
2021-02-13 17:45:57 +0900

    音量調節プログラムの処理方法を改善

--- Assets/Script/ShootingScript/SoundController.cs
+++ Assets/Script/ShootingScript/SoundController.cs
@@ -5,19 +5,15 @@ using UnityEngine.UI;
 [RequireComponent(typeof(AudioSource))]
 public class SoundController : MonoBehaviour
 {
-    //音量
-    private float _volume;
     private AudioSource _audioSource;
     void Start()
     {
-        //スライダーの値を音量に設定
         _audioSource = this.GetComponent<AudioSource>();
-        _volume = this.GetComponent<Slider>().normalizedValue;
+        ChangeVolume();
     }
-
-    void Update()
+    public void ChangeVolume()
     {
-        //スライダーで音量を変更
+        //スライダーの値を音量に設定
         _audioSource.volume = this.GetComponent<Slider>().normalizedValue;
     }
 }
