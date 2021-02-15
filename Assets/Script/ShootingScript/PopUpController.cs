fcd8595608c480a42ced39e4556436deb3439f9b
Hiraryo <nintendo640804@gmail.com>
2021-02-16 00:44:07 +0900

    アクセス修飾子の見直しとPopUpController.csを追加

---
+++ Assets/Script/ShootingScript/PopUpController.cs
@@ -0,0 +1,8 @@
+﻿//ポップアップ処理のプログラムです。
+using UnityEngine;
+
+public class PopUpController : MonoBehaviour
+{
+    [SerializeField] private GameObject _window;
+    [SerializeField] private GameObject _closeButton;
+}
